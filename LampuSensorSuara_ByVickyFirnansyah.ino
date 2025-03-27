87d6d424fdb1c6b5afcfb98c321040868fac6916
VICKYFIRNANSYAH <firnansyah15@gmail.com>
2025-03-27 15:13:47 +0700

    Add files via upload

---
+++ LampuSensorSuara_ByVickyFirnansyah.ino
@@ -0,0 +1,34 @@
+//LAMPU OTOMATIS MENGGUNAKAN SENSOR SUARA
+//BY MUHAMMAD VICKY FIRNANSYAH 
+//https://github.com/VICKYFIRNANSYAH
+
+int pinSensor = A5;
+const int Relay = 7;
+int nilaiSensor = 0;
+ 
+void setup(){
+Serial.begin (9600);
+pinMode (Relay, OUTPUT);
+digitalWrite(Relay,HIGH);
+}
+//https://github.com/VICKYFIRNANSYAH
+
+void loop() {
+nilaiSensor = analogRead(pinSensor);
+Serial.print ("Sensor = "); 
+Serial.println(nilaiSensor);
+
+if (nilaiSensor > 522){
+ delay(150);
+ while(true){
+  digitalWrite(Relay, LOW);
+  nilaiSensor = analogRead(pinSensor);
+ if(nilaiSensor > 522){break;}
+ }
+ delay(150); 
+}
+
+else{
+ digitalWrite(Relay, HIGH);
+ }
+}
